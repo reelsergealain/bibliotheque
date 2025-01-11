70a776500d7d8a2f6ff372ad6d9349baed273c00
Bleu Serge Alain <reelsergealain@gmail.com>
2025-01-11 00:31:23 +0000

    Ajout de validations pour le formulaire de livre, amélioration de l'interface avec des classes CSS, et mise à jour des URL pour inclure la fonctionnalité d'ajout de livre.

--- books/forms.py
+++ books/forms.py
@@ -8,3 +8,21 @@ class BookForm(forms.ModelForm):
         widgets = {
             'published_date': forms.DateInput(attrs={'type': 'date'})
         }
+        
+    def title_clean(self):
+        title = self.cleaned_data['title']
+        if len(title) < 10:
+            raise forms.ValidationError("Le titre doit comporter au moins 10 caractères")
+        return title
+    
+    def isbn_clean(self):
+        isbn = self.cleaned_data['isbn']
+        if len(isbn) != 13:
+            raise forms.ValidationError("L'ISBN doit comporter 13 caractères")
+        return isbn
+    
+    def quantity_clean(self):
+        quantity = self.cleaned_data['quantity']
+        if quantity < 1:
+            raise forms.ValidationError("La quantité doit être supérieure à 0")
+        return quantity
